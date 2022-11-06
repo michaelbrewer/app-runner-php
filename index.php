098d8d0c57e4e753daeac3125176ae3f96c2e7af
Michael Brewer <michael.brewer@gyft.com>
2022-11-06 01:59:52 -0800

    fix: initial fix

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
-<html>
-<head> title>First PHP App</title></head>
-<body>
-<?php
-    print("Hello World!");
-    print("<br>");
-?>
-</body>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>First PHP App</title>
+    </head>
+    <body>
+    <?php
+        print("Hello World!");
+        print("<br>");
+    ?>
+    </body>
 </html>
